6118a7ef2d2e8196c8375a4aa5ca6f418776ac9c
Akemiig <akemirodgo@gmail.com>
2024-10-18 08:19:38 -0800

    first commit

---
+++ foco.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+	int estadoFoco=0;
+	int respuesta;
+	while(1){
+		if(estadoFoco==1){
+			printf("El foco esta encendido\n");
+		}else{
+			printf("\nEl foco esta apagado\n");
+		}
+			printf("Introduce tu respuesta (1 para encender, 0 para apagar):\n");
+			scanf("%i", &respuesta);
+			
+			if(respuesta==1){
+				estadoFoco=1;
+			}else if(respuesta==0){
+				estadoFoco=0;
+			}else{
+				printf("ERROR\n");
+			}
+		}
+		return 0;
+}
